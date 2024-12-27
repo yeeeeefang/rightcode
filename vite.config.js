d4fab78b4381b74f17364e04198f227e06d5f00b
yeeeeefang <sweetcat881314@gmail.com>
2024-12-28 02:34:25 +0800

    d

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV==='production'?'/newcode/':'/',
+  // base:process.env.NODE_ENV==='production'?'/newcode/':'/',
+  base:'/newcode/',
   plugins: [react()],
 })
