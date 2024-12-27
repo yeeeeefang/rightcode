fd8247d135d297ebea504c7d4310a4a5f9c942a7
yeeeeefang <sweetcat881314@gmail.com>
2024-12-28 03:29:55 +0800

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './assets/css/App.css';
+
+//pages
+import LearnSelect from './pages/LearnSelect'
+import Trend from './pages/Trend';
+import Home from './pages/Home'
+import LearnHome from './pages/LearnHome';
+import Member from './pages/Member';
+import LearnPage from './pages/LearnPage';
+import LearnGame from './pages/LearnGame';
+import LearnGameChoose from './pages/LearnGameChoose';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import LearnArea from './pages/LearnArea';
+//components
+import Hamburger from './components/Hamburger';
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+       <Router>
+        <Routes>
+          <Route path='/' element={<Home />}></Route>
+          <Route path='/Hamburger' element={<Hamburger />}></Route>
+          <Route path='/LearnSelect' element={<LearnSelect />}></Route>
+          <Route path='/Login' element={<Login />}></Route>
+          <Route path='/LearnHome' element={<LearnHome />}></Route>
+          <Route path='/LearnPage' element={<LearnPage />}></Route>
+          <Route path='/LearnGame' element={<LearnGame />}></Route>
+          <Route path='/LearnGameChoose' element={<LearnGameChoose />}></Route>
+          <Route path='/Trend' element={<Trend />}></Route>
+          <Route path='/Member' element={<Member />}></Route>
+          <Route path='/Register' element={<Register />}></Route>
+          <Route path='/LearnArea' element={<LearnArea />}></Route>
+          {/* Add more routes as needed */}
+          {/* Example of a fallback route */}
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Routes>
+      </Router>
     </>
   )
 }
