04673fbe56345b17c5be925daafedfca6ce523ee
yeeeeefang <sweetcat881314@gmail.com>
2024-12-29 12:22:37 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // base:process.env.NODE_ENV==='production'?'/newcode/':'/',
-  base:'/newcode/',
+  base:process.env.NODE_ENV==='production'?'/newcode/':'/',
+  // base:'/newcode/',
   plugins: [react()],
   server: {
     open: true, // 啟動時自動打開瀏覽器
