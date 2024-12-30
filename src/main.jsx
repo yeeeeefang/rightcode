9ce02bed7ab8e1e3ceea85c41a9f057b311b41bf
yeeeeefang <sweetcat881314@gmail.com>
2024-12-30 14:51:55 +0800

    改名

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter  basename={process.env.NODE_ENV==='production'?'/newcode/':'/'}>
+    <BrowserRouter  basename={process.env.NODE_ENV==='production'?'/rightcode/':'/'}>
  
     <App />
     </BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:process.env.NODE_ENV==='production'?'/newcode/':'/',
+  base:process.env.NODE_ENV==='production'?'/rightcode/':'/',
   // base:'/newcode/',
   // base:'./',
   plugins: [react()],
