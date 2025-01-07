368fbd3565d4bd155838e6da1a9218aec8605eb0
TYCHIEN1120 <tychien1120@outlook.com>
2025-01-07 11:27:40 +0800

    npm i react-hook-form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,26 +2,34 @@ import { useState } from 'react';
 import "../assets/css/Login.css"
 import Navbar from "../components/Navbar"
 import { Link } from 'react-router-dom';
+import { useForm } from 'react-hook-form';
 
 export default function Login() {
 
     const [forgot, setForgot] = useState(false);
     const forgotOpen = () => { setForgot(true); }
     const forgotClose = () => { setForgot(false); }
+
+    const {
+        register, //是一種狀態(state)，可以綁定N個表單欄位
+        handleSubmit, //是一種方法
+        formState: { errors }, // 捕捉驗證規則
+    } = useForm();
+    // 建立一個字定義的方法，被表單觸發時，將register資料進來
+    const onSubmit = async (data) => {
+        // 取得表單被綁定的全部資料，以物件型態顯示
+        console.log(data);
+        // 取得表單指定的欄位資料 => data.欄位名稱
+        console.log(data.uesrpw);
+
+        await(data.email, data.uesrpw)
+    }
+
     return (
         <>
 
             <div id="login-page">
                 <Navbar />
-                {/* topbar  */}
-                {/* <header className="topbar">
-                    <div className="logo">
-                        <h1 className="logo"><img src="./images/logo.png" alt="rightCode" title="rightCode" /></h1>
-                    </div>
-                    <div className="learnBtn">
-                        <figure><img src="./images/hamBtn.svg" alt="" /></figure>
-                    </div>
-                </header> */}
                 <div className="login-main">
                     <div className="amoeba-blobs">
                         <img src="./images/login.png" alt="" />
@@ -36,16 +44,18 @@ export default function Login() {
 
                     <div className="login-form">
                         <h2>會員登入</h2>
-                        <form>
-                            <label htmlFor="email">*Email：</label>
-                            <input type="email" name="email" id="email" title="Email" placeholder="請輸入Email" required autoFocus />
-
-                            <label htmlFor="password">*請輸入密碼：</label>
-                            <input type="password" name="password" id="password" title="重設密碼" placeholder="請輸入6~8英數混合字元" />
+                        <form action='' onSubmit={handleSubmit(onSubmit)}>
+                            {/* Email */}
+                            <label htmlFor="email">Email：</label>
+                            <input type="email" id="email" title="email" placeholder="請輸入Email"
+                                {...register('email')} />
+                            {/* 使用者密碼 */}
+                            <label htmlFor="userpw">請輸入密碼：</label>
+                            <input type="password" id="userpw" title="密碼" placeholder="請輸入6~8英數混合字元" {...register('userpw')} />
 
                             <p className="forgot-password" onClick={forgotOpen}>忘記密碼</p>
-
-                            <Link to='/'><button type="submit" className="login-button">登入</button></Link> 
+                            {/* <Link to='/'></Link> */}
+                            <button type="submit" className="login-button">登入</button>
                         </form>
                         <div className="login-divider">
                             <hr />
@@ -77,9 +87,9 @@ export default function Login() {
                                     <input type="checkbox" name="remember" id="remember" title="記住密碼" value="記住密碼" />
                                     <label htmlFor="">記住這個帳號密碼</label>
                                 </div>
-                                
+
                             </form>
-                            <Link to='/Member'><p className='forgot-button'>確認</p></Link>
+                            <Link to='/Member'><button className='forgot-button'>確認</button></Link>
 
 
                         </div>
